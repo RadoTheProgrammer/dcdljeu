654ee04fd33736945994a20110ac162176b45c3e
RadoTheProgrammer <rado@arazakar.com>
2024-04-08 14:56:26 +0200

    implementation of moved .txt files to package

--- dcdljeu/mlpl.py
+++ dcdljeu/mlpl.py
@@ -1,17 +1,18 @@
-FILE_TLM="tlm_dcdl.txt"
-FILE_TLM_SORT="tlm_dcdl_sort.txt"
+
+
+import time
+import random
+import os
+
+FILE_TLM=os.path.join(os.path.dirname(__file__),"tlm_dcdl.txt")
+FILE_TLM_SORT=os.path.join(os.path.dirname(__file__),"tlm_dcdl_sort.txt")
 LETTRES="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 LETTRES_SCRABBLE=list("A"*9+"B"*2+"C"*2+"D"*3+"E"*15+"F"*2+"G"*2+"H"*2+"I"*8+"J"*1+"K"*1+"L"*5+"M"*3+
                       "N"*6+"O"*6+"P"*2+"Q"*1+"R"* 6+"S"*6+"T"*6+"U"*6+"V"*2+"W"*1+"X"*1+"Y"*1+"Z"*1)
 VOYELLES="AEIOUY"
 BS=[]
 ID_BS=[]
-FILE_BS="bs.txt"
-
-import time
-import random
-import os
-
+FILE_BS=os.path.join(os.path.dirname(__file__),"bs.txt")
 f=open(FILE_BS,"rb")
 for b in f.read():
     BS.append(bytes([b]))
